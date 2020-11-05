952427f788b8b2ffecde3d31897f5274cb7811c9
kevin <kevinx.23.96@gmail.com>
2020-11-05 11:04:26 -0500

    primer commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=m6u9j8iuysHe4STmA6CfVakB2RK0Rpoy`
+    const res = await fetch(url);
+    const { data } = await res.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_large.url
+        }
+    })
+
+    return gifs
+
+}
